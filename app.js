5df2f12209adf6e04f6f55e273e0fb7f0b0ce8e3
Chell <devchell@outlook.com>
2023-12-01 22:38:23 -0300

    Using While Loop

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 alert('Welcome to the secret number game!');
 
 let secretNumber = 10;
-console.log(secretNumber)
-let playerChoice = prompt('Select a number between 1 to 30:');
+console.log(secretNumber);
+let playerChoice;
 
-if (secretNumber == playerChoice) {
-    alert('Congrats! You guessed the secret number! (' + secretNumber + ')');
-} else {
-    alert('You missed!')
+while (playerChoice != secretNumber) {
+    playerChoice = prompt('Select a number between 1 to 30:');
+
+    if (secretNumber == playerChoice) {
+        alert(`Congrats! You guessed the secret number! (` + secretNumber + `)`);
+    } else {
+        if (playerChoice > secretNumber){
+            alert(`The secret number is less than ` + playerChoice);
+        } else {
+            alert(`The secret number is greater than ` + playerChoice);
+        }
+    }
 }
+
+//if (secretNumber == playerChoice) {
+//    alert(`Congrats! You guessed the secret number! (` + secretNumber + `)`);
+//} else {
+//    if (playerChoice > secretNumber){
+//        alert(`The secret number is less than ` + playerChoice)
+//    } else {
+//        alert(`The secret number is greater than ` + playerChoice)
+//    }
+//}
 
 
