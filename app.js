e7dfdc6db0249c833b9a16b0a2530a0646185012
Chell <devchell@outlook.com>
2023-12-01 22:54:13 -0300

    Add an attemps counter

--- app.js
+++ app.js
@@ -3,20 +3,29 @@ alert('Welcome to the secret number game!');
 let secretNumber = 10;
 console.log(secretNumber);
 let playerChoice;
+let attemps = 1;
 
 while (playerChoice != secretNumber) {
     playerChoice = prompt('Select a number between 1 to 30:');
 
     if (secretNumber == playerChoice) {
-        alert(`Congrats! You guessed the secret number! (` + secretNumber + `)`);
+        break;
     } else {
         if (playerChoice > secretNumber){
             alert(`The secret number is less than ` + playerChoice);
         } else {
             alert(`The secret number is greater than ` + playerChoice);
         }
+        attemps++;
     }
 }
+
+if (attemps > 1){
+    alert(`Congrats! You guessed the secret number! (` + secretNumber + `) with ` + attemps + ` attemps`);
+} else {
+    alert(`Congrats! You guessed the secret number! (` + secretNumber + `) with ` + attemps + ` attemp`);
+}
+
 
 //if (secretNumber == playerChoice) {
 //    alert(`Congrats! You guessed the secret number! (` + secretNumber + `)`);
